9e69b51a201b27fb51fb6e402937a7155136b93b
mugdhamadhulika <98943392+mugdhamadhulika@users.noreply.github.com>
2026-08-15 22:18:27 +0530

    Time: 1234 ms (8.40%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -8,7 +8,7 @@ SELECT
             AND pr.change_date<='2019-08-16'
         ORDER BY pr.change_date DESC LIMIT 1
     ),10) AS price
-FROM (
-    SELECT DISTINCT product_id
+FROM ( 
+    SELECT DISTINCT product_id 
     FROM products 
-) AS p
+) AS p 
